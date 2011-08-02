cf69653b1a0dfc34945bcf18a0d2ae4703109166
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2011-08-02 12:25:12 +0300

    dump sip

--- dump_sip.sh
+++ dump_sip.sh
@@ -1,2 +1,4 @@
-date=`date "+%F_%H:%M"`
-/usr/sbin/tcpdump -v -nn -s0 -i eth1 host 195.39.211.241 and udp port 5060 | tee sip-${date}.log
+LOG_DIR=~/tmp/sip_log
+DATE=`date "+%F_%H:%M"`
+
+/usr/sbin/tcpdump -v -nn -s0 -i eth1 net 195.39.211.0/24 and udp port 5060 | tee $LOG_DIR/sip-${DATE}.log
