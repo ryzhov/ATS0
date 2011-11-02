7fbe95a93f9d507de0b16916b3683940430d2637
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2011-11-02 13:36:59 +0000

    build 1.7.0-tls

--- ser/build/opensips-1.7.0-tls/lex.yy.c
+++ ser/build/opensips-1.7.0-tls/lex.yy.c
@@ -53,6 +53,7 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
+#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -82,8 +83,6 @@ typedef unsigned int flex_uint32_t;
 #ifndef UINT32_MAX
 #define UINT32_MAX             (4294967295U)
 #endif
-
-#endif /* ! C99 */
 
 #endif /* ! FLEXINT_H */
 
@@ -141,15 +140,7 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k.
- * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
- * Ditto for the __ia64__ case accordingly.
- */
-#define YY_BUF_SIZE 32768
-#else
 #define YY_BUF_SIZE 16384
-#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -1679,7 +1670,7 @@ char *yytext;
 /* config vars. */
 /* values */
 /*NUMBER		0|(([-+])?[1-9]{DIGIT}*)*/
-#line 1683 "lex.yy.c"
+#line 1674 "lex.yy.c"
 
 #define INITIAL 0
 #define STRING1 1
@@ -1765,12 +1756,7 @@ static int input (void );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k */
-#define YY_READ_BUF_SIZE 16384
-#else
 #define YY_READ_BUF_SIZE 8192
-#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -1789,7 +1775,7 @@ static int input (void );
 	if ( YY_CURRENT_BUFFER_LVALUE->yy_is_interactive ) \
 		{ \
 		int c = '*'; \
-		size_t n; \
+		unsigned n; \
 		for ( n = 0; n < max_size && \
 			     (c = getc( yyin )) != EOF && c != '\n'; ++n ) \
 			buf[n] = (char) c; \
@@ -1875,7 +1861,7 @@ YY_DECL
 
 
 
-#line 1879 "lex.yy.c"
+#line 1865 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -3420,7 +3406,7 @@ YY_RULE_SETUP
 #line 832 "cfg.lex"
 ECHO;
 	YY_BREAK
-#line 3424 "lex.yy.c"
+#line 3410 "lex.yy.c"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -4176,8 +4162,8 @@ YY_BUFFER_STATE yy_scan_string (yyconst char * yystr )
 
 /** Setup the input buffer state to scan the given bytes. The next call to yylex() will
  * scan from a @e copy of @a bytes.
- * @param yybytes the byte buffer to scan
- * @param _yybytes_len the number of bytes in the buffer pointed to by @a bytes.
+ * @param bytes the byte buffer to scan
+ * @param len the number of bytes in the buffer pointed to by @a bytes.
  * 
  * @return the newly allocated buffer state object.
  */
